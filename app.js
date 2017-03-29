5d9ef45caeb6af5d2311797d5b9e2a614dc380d3
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2017-03-29 23:18:36 +0200

    Beautify HTML,CSS and JS

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 var express = require('express'),
     github = require('octonode'),
     fs = require('fs'),
-    ids = JSON.parse(fs.readFileSync('ids.json','utf8')),
+    ids = JSON.parse(fs.readFileSync('ids.json', 'utf8')),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
     io = require('socket.io')(server),
     port = process.env.PORT || ids.port;
 
-    var client = github.client({
-      username : ids.username,
-      password : ids.password
-    });
+var client = github.client({
+    username: ids.username,
+    password: ids.password
+});
 
 //Run the server
-server.listen(port, ids.domain, function () {
-    console.log('Server host on '+ids.domain);
+server.listen(port, ids.domain, function() {
+    console.log('Server host on ' + ids.domain);
     console.log('Server listening at port %d', port);
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile('index.html');
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
 });
 
-io.on('connection', function(socket){
-  client.get('/users/benjaminbra/repos', {}, function (err, status, body, headers) {
-    socket.emit('clean');
-    for(var i=0;body!=null && i<body.length;i++){
-      repo = body[i];
-      (function(repo){
-        client.get('/repos/'+ids.username+'/'+repo.name+'/languages', {}, function(err, status, languages, headers){
-          obj = {
-            "name": repo.name,
-            "description":  repo.description,
-            "language": languages,
-            "url": repo.html_url
-          }
-          console.log(obj);
-          socket.emit('gitAdd',obj);
-        });
-      })(repo);
+io.on('connection', function(socket) {
+    client.get('/users/benjaminbra/repos', {}, function(err, status, body, headers) {
+        socket.emit('clean');
+        for (var i = 0; body != null && i < body.length; i++) {
+            repo = body[i];
+            (function(repo) {
+                client.get('/repos/' + ids.username + '/' + repo.name + '/languages', {}, function(err, status, languages, headers) {
+                    obj = {
+                        "name": repo.name,
+                        "description": repo.description,
+                        "language": languages,
+                        "url": repo.html_url
+                    }
+                    console.log(obj);
+                    socket.emit('gitAdd', obj);
+                });
+            })(repo);
 
-    }
-  });
+        }
+    });
 });
---
+++ public/benjaminbrasseur.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  //Change the backgroud color with a circle
+  $('.circle').addClass('growing-circle');
+  setTimeout(function() {
+      //Change the background color
+      $('body').addClass('started');
+      //Move name update to the top
+      $('.name').addClass('nameUpdate');
+      $('.name').addClass('started');
+      $('.main').addClass('active')
+      setTimeout(function() {
+          $('body').removeClass('started');
+          $('body').addClass('white');
+          $('.circle').addClass('remove-circle');
+      }, 500);
+  }, 1000);
+
+
+  var socket = io();
+  socket.on('clean', function() {
+      $('.main').empty();
+      $('.main').append('<div>Demo of those applications will be available soon.</div>');
+  });
+
+  socket.on('gitAdd', function(obj) {
+      description = obj.description != null ? obj.description : "Description à venir";
+      name = obj.name != null ? obj.name : "";
+      language = "";
+      if (obj.language != null) {
+          for (i in obj.language) {
+              language += '<span class="language ' + i + '">' + i + '</span>';
+          }
+      }
+      url = obj.url != null ? obj.url : "#";
+      html = '<div>' +
+          '<div class="gitRepo">' +
+          '<div class="title"><h2><a target="_blank" href="' + url + '">' + name + '</a><small> <a target="_blank" href="' + url + '"><img src="github.png" alt="Voir sur Github" /></a></small></h2></div>' +
+          language +
+          '<div class="description">' + description + '</div>' +
+          '</div>' +
+          '</div>';
+      $('.main').append(html);
+  });
+});
